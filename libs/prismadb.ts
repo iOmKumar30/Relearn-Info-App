5b185b65f453375ba13377fcdfd1210e57788270
iOmKumar30 <iomkumar03@outlook.com>
2026-05-31 18:36:03 +0530

    Migrate database from Aiven to Neon

--- libs/prismadb.ts
+++ libs/prismadb.ts
@@ -1,20 +1,32 @@
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
-import { withAccelerate } from "@prisma/extension-accelerate";
 
 const prismaClientSingleton = () => {
+  const connectionString = process.env.DATABASE_URL;
+
+  if (!connectionString) {
+    throw new Error(
+      "DATABASE_URL is not defined in your environment variables.",
+    );
+  }
+
+  const adapter = new PrismaNeon({
+    connectionString,
+  });
+
   return new PrismaClient({
-    accelerateUrl: process.env.PRISMA_ACCELERATE_URL,
-  }).$extends(withAccelerate());
+    adapter,
+  });
 };
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
+declare global {
+  var prismaGlobal: ReturnType<typeof prismaClientSingleton> | undefined;
+}
 
-export const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-
-export default prisma;
+export const prisma = global.prismaGlobal ?? prismaClientSingleton();
 
 if (process.env.NODE_ENV !== "production") {
-  globalThis.prismaGlobal = prisma;
+  global.prismaGlobal = prisma;
 }
+
+export default prisma;
